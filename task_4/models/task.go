06a6d7cd72822f7951f838c45cfd019de86b8e72
Ghaby-X <gabrielanyaele21@gmail.com>
2025-11-10 23:01:57 +0200

    update models to contain database interacting functions

--- task_4/models/task.go
+++ task_4/models/task.go
@@ -2,15 +2,75 @@
 package models
 
 import (
+	"errors"
 	"time"
 )
 
-// Tasks defines the structure of the types
-type Tasks struct {
-	ID          string    `json:"id"`
+// Task defines the structure of the types
+type Task struct {
+	ID          int       `json:"id"`
 	Title       string    `json:"title"`
-	CreatedAt   time.Time `created_at:"time"`
+	CreatedAt   time.Time `json:"created_at"`
 	Description string    `json:"description"`
 	Status      string    `json:"status"`
-	DueDate     time.Time `due_date:"string"`
+	DueDate     time.Time `json:"due_date"`
+}
+
+var (
+	Tasks  []Task
+	nextID = 1
+)
+
+func GetAllTasks() []Task {
+	return Tasks
+}
+
+// GetTaskByID : Return a task by id
+func GetTaskByID(id int) (Task, error) {
+	for _, task := range Tasks {
+		if task.ID == id {
+			return task, nil
+		}
+	}
+
+	return Task{}, errors.New("Task Not found")
+}
+
+// UpdateTask : updata tasks in database
+func UpdateTask(id int, newTask Task) error {
+	for i, task := range Tasks {
+		if task.ID == id {
+			newTask.ID = id
+			if newTask.CreatedAt.IsZero() {
+				newTask.CreatedAt = task.CreatedAt // preserve CreatedAt
+			}
+			Tasks[i] = newTask
+			return nil
+		}
+	}
+
+	return errors.New("task not found")
+}
+
+// DeleteTasks : remove tasks from database
+func DeleteTasks(id int) error {
+	newTasks := []Task{}
+
+	for _, task := range Tasks {
+		if task.ID != id {
+			newTasks = append(newTasks, task)
+		}
+	}
+
+	Tasks = newTasks
+	return nil
+}
+
+// CreateTask : creates a new task
+func CreateTask(task Task) error {
+	task.ID = nextID
+	task.CreatedAt = time.Now()
+	Tasks = append(Tasks, task)
+	nextID += 1
+	return nil
 }
