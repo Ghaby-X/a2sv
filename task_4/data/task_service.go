ebde924c4d57322dd5faa03e82f3fc29589e1b51
Ghaby-X <gabrielanyaele21@gmail.com>
2025-11-11 23:34:03 +0200

    write task service to handle get, update, create and delete

---
+++ task_4/data/task_service.go
@@ -0,0 +1,26 @@
+// Package services contains the essential services
+package services
+
+import (
+	"github.com/Ghaby-X/task_manager/models"
+)
+
+func GetTasks() []models.Task {
+	return models.GetAllTasks()
+}
+
+func GetTaskByID(id int) (models.Task, error) {
+	return models.GetTaskByID(id)
+}
+
+func CreateTask(task models.Task) error {
+	return models.CreateTask(task)
+}
+
+func UpdateTask(id int, task models.Task) error {
+	return models.UpdateTask(id, task)
+}
+
+func DeleteTask(taskID int) error {
+	return models.DeleteTasks(taskID)
+}
