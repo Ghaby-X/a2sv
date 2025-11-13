f7ce91fa00515610711266e2160bb8539d45a2fb
Ghaby-X <gabrielanyaele21@gmail.com>
2025-11-13 19:47:19 +0200

    seed database with custom task

--- task_4/main.go
+++ task_4/main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/Ghaby-X/task_manager/models"
 	"github.com/Ghaby-X/task_manager/router"
 )
 
+func SeedTasks() {
+	seed := []models.Task{}
+	dueDate := time.Now().Add(60 * time.Minute)
+	seed = append(seed, models.Task{ID: 1, Title: "first task", Description: "sample task data", Status: "Pending", CreatedAt: time.Now(), DueDate: dueDate})
+	seed = append(seed, models.Task{ID: 2, Title: "Second task", Description: "sample second task data", Status: "Approved", CreatedAt: time.Now(), DueDate: dueDate})
+
+	models.Tasks = seed
+}
+
 func main() {
+	SeedTasks()
+
 	r := router.GetTaskRouter()
 	err := r.Run()
 	if err != nil {
